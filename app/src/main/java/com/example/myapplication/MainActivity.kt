74c3ae0f08b853137126935a9ce8f9b10a93c17e
Then1ck <nickymarcellinokwan@gmail.com>
2025-05-05 18:57:12 +0700

    di bersihin navigationnya

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -20,6 +21,8 @@ class MainActivity : AppCompatActivity() {
 //        }
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.navigs)
+        bottomNav.setBackgroundColor(Color.TRANSPARENT)
+        bottomNav.elevation = 0f
 
         bottomNav.setOnItemSelectedListener { item ->
             when (item.itemId) {
